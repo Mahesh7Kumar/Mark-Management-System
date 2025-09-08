af09216253e4cba0d5c0ab6602a4ed1d31259e57
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-09-08 13:37:19 +0530

    Update comments.js

--- server/routers/comments.js
+++ server/routers/comments.js
@@ -11,10 +11,9 @@ router.get('/', async (req, res) => {
 
     const ids = studentIds.split(',').map(Number);
     const comments = await pool.query(
-  'SELECT * FROM comments WHERE student_id = ANY($1::int[]) ORDER BY created_at DESC',
-  [ids]
-);
-
+      'SELECT * FROM comparisons WHERE student_ids && $1::int[] ORDER BY created_at DESC',
+      [ids]
+    );
     res.json(comments.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
