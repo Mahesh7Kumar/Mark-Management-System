a0e3723c39ee0a7963238f72967b087535edb1c5
Mahesh <165754534+Mahesh7Kumar@users.noreply.github.com>
2025-09-08 10:41:51 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const { Pool } = pkg;
 const pool = new Pool({
-   connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+   connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
